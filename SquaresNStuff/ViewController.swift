15b38afbbed97847e27d82ab33a17be25832274c
Victoria Melendez <Victoria@Victorias-MacBook-Pro.local>
2016-10-13 13:48:04 -0400

    done

--- SquaresNStuff/ViewController.swift
+++ SquaresNStuff/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var einstein: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        einstein.layer.borderColor = UIColor.white.cgColor
+        einstein.layer.borderWidth = 3.0
+        einstein.layer.cornerRadius = 10.0
+        einstein.layer.masksToBounds = true
+        
     }
 }
+
